c4b0145a07c854c95b046dd69934f154e701895c
ykoh <ykoh@student.42seoul.kr>
2020-04-10 16:34:41 +0900

    Fix typo in ft_isalpha.c

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -6,13 +6,13 @@
 /*   By: ykoh <ykoh@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/05 14:43:16 by ykoh              #+#    #+#             */
-/*   Updated: 2020/03/05 15:26:18 by ykoh             ###   ########.fr       */
+/*   Updated: 2020/04/10 16:31:43 by ykoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isalpha (int c)
+int	ft_isalpha(int c)
 {
 	return (ft_islower(c) || ft_isupper(c));
 }
