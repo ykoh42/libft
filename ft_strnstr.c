bfb59cfd4c08005c23c84df7c82c4483c1590225
ykoh <ykoh@student.42seoul.kr>
2020-04-14 14:43:09 +0900

    Code ft_strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,17 +6,22 @@
 /*   By: ykoh <ykoh@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/04 15:18:34 by ykoh              #+#    #+#             */
-/*   Updated: 2020/04/10 23:59:45 by ykoh             ###   ########.fr       */
+/*   Updated: 2020/04/14 14:42:34 by ykoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*
-#include "stdlib.h"
+#include "libft.h"
 
-char	*strnstr(const char *haystack, const char *needle, size_t len)
+char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
+	const size_t	needle_len = ft_strlen(needle);
 
-
-
+	while (*haystack && len >= needle_len)
+	{
+		if (!ft_strncmp(haystack, needle, needle_len))
+			return ((char *)haystack);
+		haystack++;
+		len--;
+	}
+	return (NULL);
 }
-*/
