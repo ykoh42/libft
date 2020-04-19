6ff8d0f439e907e88ae621ae6b1c48750f920b17
ykoh <ykoh@student.42seoul.kr>
2020-04-20 02:14:43 +0900

    Code ft_putnbr_fd.c

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykoh <ykoh@student.42seoul.kr>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/04/17 23:54:42 by ykoh              #+#    #+#             */
+/*   Updated: 2020/04/20 02:12:44 by ykoh             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <limits.h>
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (n < 0)
+		ft_putchar_fd('-', fd);
+	if (n == INT_MIN)
+		return (ft_putstr_fd("2147483648", fd));
+	n = ft_abs(n);
+	if (n > 9)
+		ft_putnbr_fd(n / 10, fd);
+	ft_putchar_fd("0123456789"[n % 10], fd);
+}
