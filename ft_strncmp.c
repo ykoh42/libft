6700037a97353bce37b572d0c07bc7df2c847363
ykoh <ykoh@student.42seoul.kr>
2020-04-20 04:32:11 +0900

    Fix ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ykoh <ykoh@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/03 17:47:33 by ykoh              #+#    #+#             */
-/*   Updated: 2020/04/14 11:52:15 by ykoh             ###   ########.fr       */
+/*   Updated: 2020/04/20 04:30:15 by ykoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int	res;
-
 	while (n--)
-		if ((res = (unsigned char)*s1++ - (unsigned char)*s2++) || *s1 == '\0')
-			return (res);
+	{
+		if (*s1++ != *s2++)
+			return (*(unsigned char *)(s1 - 1) - *(unsigned char *)(s2 - 1));
+		if (*(s1 - 1) == '\0')
+			break ;
+	}
 	return (0);
 }
