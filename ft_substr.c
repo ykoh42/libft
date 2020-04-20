1c0d942f569c29d8501e59222d18e3facefa3e6b
ykoh <ykoh@student.42seoul.kr>
2020-04-21 00:16:59 +0900

    Fix ft_substr.c to return empty string if over index

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: ykoh <ykoh@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/14 15:47:00 by ykoh              #+#    #+#             */
-/*   Updated: 2020/04/14 16:40:33 by ykoh             ###   ########.fr       */
+/*   Updated: 2020/04/21 00:15:12 by ykoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,12 @@
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	char sub_str[len + 1];
+	char	sub_str[len + 1];
 
 	if (!s)
 		return (NULL);
+	if (ft_strlen(s) + 1 < start + len)
+		return (ft_strdup(""));
 	ft_strlcpy(sub_str, s + start, len + 1);
 	return (ft_strdup(sub_str));
 }
