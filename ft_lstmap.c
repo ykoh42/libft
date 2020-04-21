f6cfd5c723ec7f54689fbd470f3e247ae277f3d2
ykoh <ykoh@student.42seoul.kr>
2020-04-21 15:08:02 +0900

    Fix ft_lstmap.c not to clear origin lst

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: ykoh <ykoh@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/06 21:04:49 by ykoh              #+#    #+#             */
-/*   Updated: 2020/04/08 20:55:39 by ykoh             ###   ########.fr       */
+/*   Updated: 2020/04/21 15:07:16 by ykoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,15 +16,23 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	const t_list	*lstp = lst;
 	void			*is_success;
+	t_list			*ret;
 	t_list			*new;
 
+	ret = NULL;
 	new = NULL;
 	while (lstp)
 	{
 		if ((is_success = f(lstp->content)))
-			ft_lstadd_back(&new, ft_lstnew(is_success));
+		{
+			if (!(new = ft_lstnew(is_success)))
+			{
+				ft_lstclear(&lst, del);
+				return (NULL);
+			}
+			ft_lstadd_back(&ret, new);
+		}
 		lstp = lstp->next;
 	}
-	ft_lstclear(&lst, del);
-	return (new);
+	return (ret);
 }
