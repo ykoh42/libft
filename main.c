160ac8478694f1f96c2c20d1ed4d45400cff7823
ykoh <ykoh@student.42seoul.kr>
2020-05-08 03:33:31 +0900

    Remove main.c

--- main.c
+++
@@ -1,9 +0,0 @@
-#include "libft.h"
-#include <string.h>
-#include <stdio.h>
-
-int main()
-{
-	printf("strnstr : %s\n", strnstr("", "asdf", 100));
-	printf("strnstr : %s\n", ft_strnstr("", "asdf", 100));
-}
