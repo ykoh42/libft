35f30e271db36ba89eed363bc9cd91ea1f589e1d
ykoh <ykoh@student.42seoul.kr>
2020-05-11 23:42:42 +0900

    Fix typo

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: ykoh <ykoh@student.42seoul.kr>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/04/14 16:31:35 by ykoh              #+#    #+#             */
-/*   Updated: 2020/05/05 07:52:55 by ykoh             ###   ########.fr       */
+/*   Updated: 2020/05/30 12:43:57 by ykoh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	if (!s1 || !s2)
 		return (NULL);
 	str_len = ft_strlen(s1) + ft_strlen(s2);
-	if (!(str = malloc(str_len + 1 * sizeof(char))))
+	if (!(str = malloc((str_len + 1) * sizeof(char))))
 		return (NULL);
 	ft_strlcpy(str + ft_strlcpy(str, s1, str_len + 1), s2, str_len + 1);
 	return (str);
